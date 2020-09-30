c60266ba947590939e05c3bfb1511360a7eb7db8
chic-state-0895 <brookeabryski@gmail.com>
2020-09-30 20:57:18 +0000

    add validation check

--- lib/art_pieces/cli.rb
+++ lib/art_pieces/cli.rb
@@ -1,20 +1,29 @@
 class ArtPieces::CLI 
   def call
-    puts "Welcome to your art lesson!"
-    #get_art_pieces
-    # list_art_pieces
-    #get_information_for(art_piece)
+    puts "\nWelcome to your art lesson!\n"
+    get_art_pieces
+    list_art_pieces
+    get_user_art_piece
   end
   
   def get_art_pieces
-    # to be scraped instead
     @art_pieces = ['Mona Lisa by Leonardo da Vinci', 'Mona Lisa by Leonardo da Vinci']
   end
   
   def list_art_pieces
-    #list art pieces
+    puts "Choose a piece of art to learn more about."
     @art_pieces.each.with_index(1) do |art_piece, index| 
-    puts "#{index + 1}. #{art_piece}"
+    puts "#{index}. #{art_piece}"
     end
+  end
+  
+  def get_user_art_piece
+    chosen_art_piece = gets.strip
+    #if valid_input(chosen_art_piece,@art_pieces)
+    #end
+  end
+  
+  def valid_input(input, data)
+    input.to_i <= data.length && input.to_i > 0
   end
 end

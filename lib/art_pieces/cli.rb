6141cedfb824059f175652f5d7113eff17fde4f5
chic-state-0895 <brookeabryski@gmail.com>
2020-09-30 20:11:46 +0000

    start cli stub

--- lib/art_pieces/cli.rb
+++ lib/art_pieces/cli.rb
@@ -1,6 +1,20 @@
 class ArtPieces::CLI 
   def call
     puts "Welcome to your art lesson!"
-    binding.pry
+    #get_art_pieces
+    # list_art_pieces
+    #get_information_for(art_piece)
+  end
+  
+  def get_art_pieces
+    # to be scraped instead
+    @art_pieces = ['Mona Lisa by Leonardo da Vinci', 'Mona Lisa by Leonardo da Vinci']
+  end
+  
+  def list_art_pieces
+    #list art pieces
+    @art_pieces.each.with_index(1) do |art_piece, index| 
+    puts "#{index + 1}. #{art_piece}"
+    end
   end
 end

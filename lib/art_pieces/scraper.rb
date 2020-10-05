9f1ffa37711df293b5f0b589a7ed4c5ccbceb63e
github username <github email address>
2020-10-05 11:19:53 -0700

    potential new scraper

--- lib/art_pieces/scraper.rb
+++ lib/art_pieces/scraper.rb
@@ -1,30 +1,48 @@
+
 class ArtPieces::Scraper
   
   def self.scrape_paintings
-    doc = Nokogiri::HTML(open("https://historylists.org/art/20-of-the-world%E2%80%99s-most-famous-art-pieces.html"))
+    doc = Nokogiri::HTML(open("https://www.brushwiz.com/most-famous-paintings/"))
     
-     paintings = doc.css("body h2")
+    paintings = doc.css("div.row.items-grid.grid-categories.page-section div.col.col-25")
   
- paintings.each do |p|
-  name = p.text
-  ArtPieces::Painting.new(name)
-    end
+  paintings.each do |painting|
+name = painting.text.strip
+url = painting.css("a").attr("href").value
+ArtPieces::Painting.new(name)
   end
+end
   
-
-  def self.scrape_details(painting)
-    url = "https://historylists.org/art/20-of-the-world%E2%80%99s-most-famous-art-pieces.html"
-   doc = Nokogiri::HTML(open(url))
+    #def self.scrape_details(chosen_painting)
     
-    details = doc.css("body p")
+    #details = doc.css("body p")
     
-   details.each do |p|
-     description = p.text.strip
-      painting.details << description
-   end
-  end
+   #details.each do |p|
+    #description = p.text.strip
+    #  painting.details << description
+   #end
+  #end
  
 
-  
-end
+ # def self.scrape_details(painting)
+  #  url = "https://historylists.org/art/20-of-the-world%E2%80%99s-most-famous-art-pieces.html"
+ #  doc = Nokogiri::HTML(open(url))
+    
+  #  details = doc.css("body p")
+    
+  # details.each do |p|
+   #  description = p.text.strip
+   #   painting.details << description
+  # end
+ # end
+ 
 
+def self.scrape_details(chosen_painting)
+  url = "https://www.brushwiz.com#{painting.url}
+  doc = Nokogiri::HTML(open(url))
+  p = doc.css("div.product-block.large")
+  p.each do |p|
+   description = p.text.strip
+   painting.details << description
+  end
+  
